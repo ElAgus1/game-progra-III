c69447dd2a35692f36ab611372b3b2db2049edda
Agustin Davila <agustindavila637@gmail.com>
2024-05-21 22:01:26 -0300

    Blue area added

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,49 +1,12 @@
 "use client";
 // components/Sidebar.js
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import building1 from "./imagenes/image3.webp";
+import SideBar from "./sideBar";
 
-const Sidebar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
-
-  return (
-    <div className="bg-blue-200">
-      <div
-        className={`fixed top-0 left-0 h-full bg-gray-900  text-white transition-all flex flex-col ${
-          isOpen ? "w-64" : "w-16"
-        }`}
-      >
-        <button
-          onClick={toggleSidebar}
-          className="bg-gray-800 p-4 focus:outline-none"
-        >
-          {isOpen ? "<" : ">"}
-        </button>
-        <nav className="flex-1 mt-10">
-          <ul className="space-y-2 flex flex-col h-full">
-            <li>
-              <a href="/about" className="block px-4 py-2 hover:bg-gray-700">
-                {isOpen ? "bulding 1" : ""}
-              </a>
-            </li>
-            <li>
-              <a href="/contact" className="block px-4 py-2 hover:bg-gray-700">
-                {isOpen ? "bulding 2" : ""}
-              </a>
-            </li>
-            <li className="">
-              <a href="/" className="block px-4 py-2 hover:bg-gray-700">
-                {isOpen ? "bulding 3" : ""}
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  );
+const Page = () => {
+  return <SideBar />;
 };
 
-export default Sidebar;
+export default Page;
